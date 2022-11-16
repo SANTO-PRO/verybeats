59cf63a8400924d2ceebd86bff2095011fe9725c
Saidul Islam Santo <sksanto.pro@gmail.com>
2022-11-16 16:53:00 +0600

    addUp

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 class DrumKit {
 	constructor() {
+		// Buttons And Pads:
 		this.pads = document.querySelectorAll('.pad');
 		this.palyBtn = document.querySelector('.play-btn');
 		this.palyBtnIcon = document.querySelector('.fa-play');
 		this.stopBtnIcon = document.querySelector('.fa-stop');
+		this.selects = document.querySelectorAll('select');
+		this.muteBtns = document.querySelectorAll('.mute');
+		this.resetBtn = document.querySelector('.clear');
+		this.bpmSlider = document.querySelector('.beatPerMin-slider');
+
+		// Sounds:
 		this.kickAudio = document.querySelector('.kick-sound');
 		this.hihatAudio = document.querySelector('.hihat-sound');
 		this.snareAudio = document.querySelector('.snare-sound');
 		this.openhihatAudio = document.querySelector('.hihat-open-sound');
 		this.clapAudio = document.querySelector('.clap-sound');
 
+		this.currentKick = './sounds/kicks/kick-heavy.wav';
+		this.currentHihat = './sounds/hihats/hihat-808.wav';
+		this.currentOpenhat = './sounds/openhats/openhat-analog.wav';
+		this.currentSnare = './sounds/snares/snare-acoustic01.wav';
+		this.currentClap = './sounds/claps/clap-crushed.wav';
+
+		// Others:
 		this.index = 0;
 		this.bpm = 150;
 		this.isPlaying = null;
@@ -51,7 +65,7 @@ class DrumKit {
 				}
 
 				if (pad.classList.contains('hihat-open-pad')) {
-					this.hihatAudio.currentTime = 0;
+					this.openhihatAudio.currentTime = 0;
 					this.openhihatAudio.play();
 				}
 
@@ -88,11 +102,102 @@ class DrumKit {
 
 			this.palyBtnIcon.style.display = 'block';
 			this.stopBtnIcon.style.display = 'none';
+
+			this.index = 0;
 		}
+	}
+
+	changeSound(event) {
+		const trackName = event.target.name;
+		const trackValue = event.target.value;
+
+		switch (trackName) {
+			case 'kick-select':
+				this.kickAudio.src = trackValue;
+				break;
+			case 'snare-select':
+				this.snareAudio.src = trackValue;
+				break;
+			case 'hihat-select':
+				this.hihatAudio.src = trackValue;
+				break;
+			case 'hihat-open-select':
+				this.openhihatAudio.src = trackValue;
+				break;
+			case 'clap-select':
+				this.clapAudio.src = trackValue;
+				break;
+		}
+	}
+
+	muteSound(event) {
+		const muteIndex = event.target.getAttribute('data-track');
+
+		event.target.children[0].classList.toggle('active');
+		event.target.children[1].classList.toggle('active');
+
+		event.target.classList.toggle('active');
+
+		if (event.target.classList.contains('active')) {
+			switch (muteIndex) {
+				case '0':
+					this.kickAudio.volume = 0;
+					break;
+				case '1':
+					this.hihatAudio.volume = 0;
+					break;
+				case '2':
+					this.snareAudio.volume = 0;
+					break;
+				case '3':
+					this.openhihatAudio.volume = 0;
+					break;
+				case '4':
+					this.clapAudio.volume = 0;
+					break;
+			}
+		} else {
+			switch (muteIndex) {
+				case '0':
+					this.kickAudio.volume = 1;
+					break;
+				case '1':
+					this.hihatAudio.volume = 1;
+					break;
+				case '2':
+					this.snareAudio.volume = 1;
+					break;
+				case '3':
+					this.openhihatAudio.volume = 1;
+					break;
+				case '4':
+					this.clapAudio.volume = 1;
+					break;
+			}
+		}
+	}
+
+	changeBpm(event) {
+		this.bpm = event.target.value;
+
+		clearInterval(this.isPlaying);
+		this.isPlaying = null;
+
+		if (this.palyBtn.classList.contains('active')) {
+			this.start();
+		}
+	}
+
+	changeBpmText(event) {
+		const bpmText = document.querySelector('.beatPerMin-value');
+
+		bpmText.innerText = event.target.value;
 	}
 }
 
 const drumKit = new DrumKit();
+
+// >>===> EventListeners:
 
 drumKit.pads.forEach((pad) => {
 	pad.addEventListener('click', drumKit.activePad);
@@ -104,4 +209,30 @@ drumKit.pads.forEach((pad) => {
 drumKit.palyBtn.addEventListener('click', () => {
 	drumKit.start();
 	drumKit.updateBtn();
+});
+
+drumKit.selects.forEach((select) => {
+	select.addEventListener('change', function (event) {
+		drumKit.changeSound(event);
+	});
+});
+
+drumKit.muteBtns.forEach((btn) => {
+	btn.addEventListener('click', function (event) {
+		drumKit.muteSound(event);
+	});
+});
+
+drumKit.resetBtn.addEventListener('click', () => {
+	drumKit.pads.forEach((pad) => {
+		pad.classList.remove('active');
+	});
+});
+
+drumKit.bpmSlider.addEventListener('input', function (event) {
+	drumKit.changeBpmText(event);
+});
+
+drumKit.bpmSlider.addEventListener('change', function (event) {
+	drumKit.changeBpm(event);
 });
